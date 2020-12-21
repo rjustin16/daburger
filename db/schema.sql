9c6f3a57ce19301c43ec1324dd738761bf827dfb
Ricky Justiniano <justiniano.ricky@gmail.com>
2020-12-21 09:32:57 -0700

    push

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   burger_name VARCHAR(30) NOT NULL,
-  devoured BOOLEAN DEFAULT false
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 )
