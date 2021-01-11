7c806aa7a2154c6cf59ff88d4a66d6c7c048c930
rjustin16 <justiniano.ricky@gmail.com>
2021-01-11 13:03:41 -0700

    working hopefully

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
@@ -5,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers(
   id INT AUTO_INCREMENT,
   burger_name VARCHAR(30) NOT NULL,
-  eaten BOOLEAN DEFAULT false,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 )
